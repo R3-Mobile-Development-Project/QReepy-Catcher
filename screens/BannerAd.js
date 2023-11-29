04d72a9a13342a546ed251ced05bd3e8686acd87
anssikulotie <anssi.kulotie@hotmail.com>
2023-11-29 21:57:35 +0200

    Support to add more ads to banner

--- screens/BannerAd.js
+++ screens/BannerAd.js
@@ -1,59 +1,87 @@
-import React, { useEffect } from 'react';
-import { View, Text, StyleSheet, Animated, Easing,TouchableOpacity,Linking } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, Text, StyleSheet, Animated, Easing, TouchableOpacity, Linking } from 'react-native';
+
+
+const ads = [
+  {
+    text: "Life's too short to drink water! Grab a glass of fine whiskey and start turning your ordinary moments into extraordinary memories. Remember, whiskey may not solve all your problems, but it's worth a shot (or two)!😄🥃" ,
+    url: 'https://en.wikipedia.org/wiki/Alcoholism',
+    displayDuration: 24000, // e.g., 26 seconds
+    backgroundColor: '#E38200', // Example color
+    
+  },
+  {
+    text: "Finnair: Where the only thing we take more seriously than your safety is our ability to lose your luggage with a smile!✈️"    ,
+    url: 'https://www.finnair.com/fi-en',
+    displayDuration: 14000, // e.g., 13 seconds
+    backgroundColor: '#add8e6', // Another example color
+
+  },
+  {
+    text: "Listen to this masterpiece while you're at it!🎶" ,
+    url: 'https://www.youtube.com/watch?v=Dc6pAuRiyVo&t=4s',
+    displayDuration: 8000, // e.g., 10 seconds
+    backgroundColor: '#FF0000', // Example color
+    
+  },
+  // Add more ads with their durations as needed
+];
 
 const BannerAd = () => {
+  const [currentAdIndex, setCurrentAdIndex] = useState(0);
   const animatedValue = new Animated.Value(0);
-
-  useEffect(() => {
-    animateBannerAd();
-  }, []);
 
   const animateBannerAd = () => {
     animatedValue.setValue(0);
     Animated.timing(animatedValue, {
       toValue: 1,
-      duration: 26000, // Adjust duration as needed
+      duration: 26000, // Fixed duration for scrolling speed
       easing: Easing.linear,
-      useNativeDriver: false,
-    }).start(() => {
-      setTimeout(animateBannerAd, 1000); // Delay before restart
-    });
+      useNativeDriver: true,
+    }).start();
   };
-  const openURL = () => {
-    Linking.openURL('https://fi.wikipedia.org/wiki/Alkoholismi')
-      .catch(err => console.error('An error occurred', err));
-  };
-  
 
+  useEffect(() => {
+    animateBannerAd();
+    const interval = setTimeout(() => {
+      setCurrentAdIndex((prevIndex) => (prevIndex + 1) % ads.length);
+    }, ads[currentAdIndex].displayDuration); // Use the duration from the ad data
+
+    return () => clearTimeout(interval);
+  }, [currentAdIndex]);
+
+  const openURL = () => {
+    const url = ads[currentAdIndex].url;
+    Linking.openURL(url).catch(err => console.error('An error occurred', err));
+  };
 
   return (
-    <View style={styles.adContainer}>
-          <TouchableOpacity onPress={openURL}>
-
+    <View style={[styles.adContainer, { backgroundColor: ads[currentAdIndex].backgroundColor }]}>
+    <TouchableOpacity onPress={() => openURL(ads[currentAdIndex].url)}>
       <Animated.View
-        style={[
-          styles.adContent,
-          {
-            transform: [
-              {
-                translateX: animatedValue.interpolate({
-                  inputRange: [0, 1],
-                  outputRange: [300, -3350], // Adjust based on actual text and container width
-                }),
-              },
-            ],
-          },
-        ]}
-      >
-        <Text style={styles.adText}>
-          "Life's too short to drink water! Grab a glass of fine whiskey and start turning your ordinary moments into extraordinary memories. Remember, whiskey may not solve all your problems, but it's worth a shot (or two)!" 😄🥃
-        </Text>
-      </Animated.View>
+          style={[
+            styles.adContent,
+            {
+              transform: [
+                {
+                  translateX: animatedValue.interpolate({
+                    inputRange: [0, 1],
+                    outputRange: [300, -3350], // Adjust as needed to accommodate the longest text
+                  }),
+                },
+              ],
+            },
+          ]}
+        >
+          <Text style={styles.adText}>
+            {ads[currentAdIndex].text}
+          </Text>
+        </Animated.View>
       </TouchableOpacity>
-
     </View>
   );
 };
+
 
 const styles = StyleSheet.create({
   adContainer: {
@@ -67,8 +95,9 @@ const styles = StyleSheet.create({
     paddingHorizontal: 10,
   },
   adText: {
-    fontSize: 34,
+    fontSize: 30,
     fontWeight: 'bold',
+    fontStyle: 'italic',
     width: '1000%', // Set a very wide width to ensure text flows horizontally
   },
 });
