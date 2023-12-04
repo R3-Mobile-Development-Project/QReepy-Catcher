89c730bb511e7f7f274cafe1990ca2ce1d9b4d70
Niko <t1koni02@students.oamk.fi>
2023-12-04 16:21:14 +0200

    monsterinfomodali muutoksia

--- modals/MonsterInfoModal.js
+++ modals/MonsterInfoModal.js
@@ -1,4 +1,4 @@
-import { Modal, View, Text, StyleSheet, Button, TouchableOpacity, Image } from 'react-native';
+import { Modal, View, Text, StyleSheet, Button, TouchableOpacity, Image, ScrollView } from 'react-native';
 import { MaterialIcons } from '@expo/vector-icons';
 import { Audio } from 'expo-av';
 import React, { useState, useEffect } from 'react';
@@ -34,57 +34,84 @@ const MonsterInfoModal = ({ isModalVisible, selectedMonster, onClose }) => {
 
   
  return (
-  <Modal
-  animationType="slide"
-  transparent={true}
-  visible={isModalVisible}
-  onRequestClose={() => isModalVisible(false)}
->
-<View style={{...styles.modalView, backgroundColor: backgroundColor}}>
-    {selectedMonster && (
-      <>
-        <Image source={{ uri: selectedMonster.image }} style={styles.image} />
-        <View style={styles.textContainer}>
- <Text style={styles.monsterName}>{selectedMonster.name}</Text>
- <Text style={styles.rarity}>Rarity: {selectedMonster.rarity}</Text>
- <Text style={styles.age}>Age: {selectedMonster.age}</Text>
- <Text style={styles.title}>Title: {selectedMonster.title}</Text>
- <Text style={styles.nature}>Nature: {selectedMonster.nature}</Text>
- <Text style={styles.background}>Background: {selectedMonster.background}</Text>
-</View>
-      </>
-    )}
-    <TouchableOpacity onPress={handleClosePress} style={styles.closeButton}>
-          <MaterialIcons name="close" size={50} color="black" />
+    <Modal
+    animationType="fade"
+    transparent={true}
+    visible={isModalVisible}
+    onRequestClose={() => isModalVisible(false)}
+    >
+      <View style={styles.modalContainer}>
+        <View style={{ ...styles.modalContent, backgroundColor: backgroundColor }}>
+          {selectedMonster && (
+            <>
+              <Image source={{ uri: selectedMonster.image }} style={styles.image} />
+              <ScrollView contentContainerStyle={styles.scrollViewContent}>
+                <View style={styles.rowContainer}>
+                  <Text style={styles.monsterName}>{selectedMonster.name}</Text>
+                  <Text style={styles.title}>{selectedMonster.title}</Text>
+                </View>
+
+                <Text style={styles.rarity}>{selectedMonster.rarity} QReep</Text>
+                <Text style={styles.age}>Age: {selectedMonster.age}</Text>
+                <Text style={styles.nature}>Nature: {selectedMonster.nature}</Text>
+                <Text style={styles.background}>{selectedMonster.background}</Text>
+                {/* Add more monster details here */}
+              </ScrollView>
+            </>
+          )}
+          <TouchableOpacity onPress={handleClosePress} style={styles.closeButton}>
+            <MaterialIcons name="close" size={50} color="black" />
           </TouchableOpacity>
-  </View>
-  
-</Modal>
+        </View>
+      </View>
+      
+    </Modal>
  );
 };
 
 const styles = StyleSheet.create({
-  modalView: {
+  rowContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom: -4,
+  },
+  scrollViewContent: {
+    flexGrow: 1,
+    paddingBottom: 80,
+  },
+  modalContainer: {
     flex: 1,
-    margin: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.7)',
+  },
+  modalContent: {
+    width: '90%',
+    height: '85%',
+    padding: 20,
     borderRadius: 20,
-    padding: 35,
-    alignItems: "center",
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 2
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 4,
-    elevation: 5,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  scrollContainer: {
+    flex: 1,
+    width: '80%',
+    height: '80%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.7)',
+  },
+  modalView: {
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   closeButton: {
     position: 'absolute',
-    bottom: 20,
+    bottom: 0,
     width: 70,
     height: 70,
-    borderRadius: 25,
+    borderRadius: 35,
     borderColor: 'black',
     borderWidth: 3,
     backgroundColor: 'salmon',
@@ -99,7 +126,7 @@ const styles = StyleSheet.create({
     width: 300, // Set the image width to 100% to fit the container
     height: 300,
     marginBottom: 20,
-    borderRadius: 90,
+    borderRadius: 30,
     borderWidth: 2,
     borderColor: 'black',
   },
@@ -108,34 +135,35 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
   monsterName: {
-    fontSize: 20,
+    fontSize: 24,
     fontWeight: 'bold',
     marginBottom: 20,
     backgroundColor: 'rgba(255, 255, 255, 0.3)', // Transparent white background
         paddingLeft: 4,
         paddingRight: 4,
         borderRadius: 8, // Add border radius for rounded corners
-    
   },
   rarity: {
+    alignSelf: 'center',
+    fontWeight: 'bold',
     fontSize: 18,
-    marginBottom: 20,
+    marginBottom: 10,
     backgroundColor: 'rgba(255, 255, 255, 0.3)', // Transparent white background
         paddingLeft: 4,
         paddingRight: 4,
         borderRadius: 8, // Add border radius for rounded corners
-    
   },
   age: {
-    fontSize: 16,
-    marginBottom: 20,
+    fontSize: 20,
+    marginBottom: 12,
     backgroundColor: 'rgba(255, 255, 255, 0.3)', // Transparent white background
         paddingLeft: 4,
         paddingRight: 4,
         borderRadius: 8, // Add border radius for rounded corners
   },
   title: {
-    fontSize: 18,
+    fontSize: 20,
+    fontWeight: '500',
     marginBottom: 20,
     backgroundColor: 'rgba(255, 255, 255, 0.3)', // Transparent white background
         paddingLeft: 4,
@@ -143,15 +171,16 @@ const styles = StyleSheet.create({
         borderRadius: 8, // Add border radius for rounded corners
   },
   nature: {
-    fontSize: 16,
-    marginBottom: 20,
+    fontSize: 20,
+    marginBottom: 12,
     backgroundColor: 'rgba(255, 255, 255, 0.3)', // Transparent white background
         paddingLeft: 4,
         paddingRight: 4,
         borderRadius: 8, // Add border radius for rounded corners
   },
   background: {
-    fontSize: 14,
+    fontSize: 22,
+    fontWeight: '400',
     color: 'black',
     backgroundColor: 'rgba(255, 255, 255, 0.3)', // Transparent white background
         paddingLeft: 4,
